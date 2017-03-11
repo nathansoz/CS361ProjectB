fe98f74a8966a964e3a5af3959e98052aa10ea88
James Sandoval <gunnisurf@gmail.com>
2017-03-11 11:14:49 -0800

    front end shell created with support for registration/login and verification.

--- src/app.js
+++ src/app.js
@@ -120,9 +120,9 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
   if(process.env['FBDEV']) {
     console.log('Dev environment variable set! Using dev settings...');
-    //console.log("No Database Connection.\nRemove comments from lines 122-123(app.js) to connect to mysql")
+    console.log("No Database Connection.\nRemove comments from lines 122-123(app.js) to connect to mysql")
 
-    console.log('Creating sample user');
-    db.init(function() { seed() });
+    //console.log('Creating sample user');
+    //db.init(function() { seed() });
   }
 })
